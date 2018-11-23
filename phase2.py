4aac48c2f28ede6570c64cd7fcca338d8a060246
FRANK-SURFACE\tomcm <tmaurer@ualberta.ca>
2018-11-23 14:29:49 -0700

    checking if phase2 is finished

---
+++ phase2.py
@@ -0,0 +1,75 @@
+import os
+import re
+
+def main():
+
+    # Sort files
+    os.system("sort -u -o sortedPrices.txt prices.txt")
+    os.system("sort -n -o sortedPrices.txt sortedPrices.txt")
+    os.system("sort -u -o sortedAds.txt ads.txt")
+    os.system("sort -u -o sortedPdates.txt pdates.txt")
+    os.system("sort -u -o sortedTerms.txt terms.txt")
+
+    # Scrub the Ad file
+    inputAdFile = open("sortedAds.txt",'r')
+    outputAdFile = open("readyAds.txt",'w')
+
+    for line in inputAdFile:
+        items = line.split(":<ad>")
+        if len(items) == 2:
+            cleaned = re.sub(r"\\", r"\\\\", items[1])
+            outputString = items[0] + "\n" + "<ad>" + cleaned
+            outputAdFile.write(outputString)
+
+    inputAdFile.close()
+    outputAdFile.close()
+
+    # Scrub the prices file
+    inputAdFile = open("sortedPrices.txt",'r')
+    outputAdFile = open("readyPrices.txt",'w')
+
+    for line in inputAdFile:
+        items = line.split(":")
+        if len(items) == 2:
+            cleaned = re.sub(r"\\", r"\\\\", items[1])
+            outputString = items[0] + "\n" + cleaned
+            outputAdFile.write(outputString)
+
+    inputAdFile.close()
+    outputAdFile.close()
+
+    # Scrub the pdates file
+    inputAdFile = open("sortedPdates.txt",'r')
+    outputAdFile = open("readyPdates.txt",'w')
+
+    for line in inputAdFile:
+        items = line.split(":")
+        if len(items) == 2:
+            cleaned = re.sub(r"\\", r"\\\\", items[1])
+            outputString = items[0] + "\n" + cleaned
+            outputAdFile.write(outputString)
+
+    inputAdFile.close()
+    outputAdFile.close()
+
+    # Scrub the terms file
+    inputAdFile = open("sortedTerms.txt",'r')
+    outputAdFile = open("readyTerms.txt",'w')
+
+    for line in inputAdFile:
+        items = line.split(":")
+        if len(items) == 2:
+            cleaned = re.sub(r"\\", r"\\\\", items[1])
+            outputString = items[0] + "\n" + cleaned
+            outputAdFile.write(outputString)
+
+    inputAdFile.close()
+    outputAdFile.close()
+
+    # Create indexes
+    os.system("db_load -f readyAds.txt -t hash ad.idx")
+    os.system("db_load -f readyPdates.txt -t btree da.idx")
+    os.system("db_load -f readyTerms.txt -t btree te.idx")
+    os.system("db_load -f readyPrices.txt -t btree pr.idx")
+
+main()
