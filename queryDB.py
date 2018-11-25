720f1edaf74090115ea229d90a307e3104cea132
Unknown <ltyue@ualberta.ca>
2018-11-25 15:53:35 -0700

    queryDB is complete
    doesn't handle no whitespace yet. Also, will not function properly until singleQuery is complete

--- queryDB.py
+++ queryDB.py
@@ -4,83 +4,116 @@ import re
 
 
 def main():
+	full = False
+	exit = False
 
-	search = input("Input Query: ").lower()
-	search_words = search.split()
-	num_words = len(search_words)
-	valid_query = True
-	current_word = 0
-	data = []
 
-	while current_word < num_words:
-		# print(search_words[current_word])
-		word = search_words[current_word]
+	while not exit:
+		search = input("Input Query: ").lower()
 
-		# check if query is date
-		if (word == "date"):
-			operator = search_words[current_word + 1]
-			target = search_words[current_word + 2]
+		if (search == "output=full"):
+			full = True
+			continue
 
-			if check_operator(operator):
-				
-				try: 
-					year,month,day = target.split('/')
-					datetime.datetime(int(year), int(month), int(day))
+		elif (search == "output=brief"):
+			full = False
+			continue
 
-				except ValueError:
+		elif (search == "exit"):
+			break
+
+		search_words = search.split()
+		num_words = len(search_words)
+		valid_query = True
+		current_word = 0
+		data = []
+
+		while current_word < num_words:
+			# print(search_words[current_word])
+			word = search_words[current_word]
+
+			# check if query is date
+			if (word == "date"):
+				operator = search_words[current_word + 1]
+				target = search_words[current_word + 2]
+
+				if check_operator(operator):
+					
+					try: 
+						year,month,day = target.split('/')
+						datetime.datetime(int(year), int(month), int(day))
+
+					except ValueError:
+						valid_query = False
+						break
+
+					# search_query = word + " " + operator + " " + target
+					search_query = word + operator + target
+					# print(search_query)
+					data = query(search_query, data)
+					current_word += 3	
+
+				else:
 					valid_query = False
 					break
 
-				# search_query = word + " " + operator + " " + target
-				search_query = word + operator + target
-				# print(search_query)
-				data = query(search_query)
-				current_word += 3	
+			# check if query is price
+			elif (word == "price"):
+				operator = search_words[current_word + 1]
+				target = search_words[current_word + 2]
+
+				if (check_operator(operator)):
+					try:
+						int(target)
+
+					except ValueError:
+						valid_query = False
+						break
+
+					search_query = word + operator + target
+					data = query(search_query, data)
+					current_word += 3
+
+				else:
+					valid_query = False
+					break
+
+			elif (word == "location" or word == "cat"):
+				operator = search_words[current_word + 1]
+				target = search_words[current_word + 2]
+
+				if (check_operator(operator)):
+					search_query = word + operator + target
+
+					data = query(search_query, data)
+					current_word += 3
+
+
+
+
+			# check if query is term
+			elif (re.match(r'^[a-z0-9]+[%]?$', word) is not None):	#Fred assisted with RegEx understanding
+				data = query(word, data)
+				current_word += 1
 
 			else:
 				valid_query = False
 				break
+			
+		if (valid_query):
+			# print("Valid Query")
 
-		# check if query is price
-		elif (word == "price"):
-			operator = search_words[current_word + 1]
-			target = search_words[current_word + 2]
-
-			if (check_operator(operator)):
-				try:
-					int(target)
-
-				except ValueError:
-					valid_query = False
-					break
-
-				search_query = word + operator + target
-				data = query(search_query)
-				current_word += 3
+			if full:
+				for value in data:
+					# entry + " | " + date + " | " + loc + " | " + cat + " | " + title + " | " + desc + " | " + price
+					print(value[0] + " | " + value[1] + " | " + value[2] + " | " + value[3] + " | " + value[4] + " | " + value[5] + " | " + value[6])
 
 			else:
-				valid_query = False
-				break
-
-		# check if query is camera
-		elif (re.match(r'^[a-z0-9]+[%]?$', word) is not None):
-			data = query(word)
-			current_word += 1
+				for value in data:
+					print(value[0] + " | " + value[1])
 
 		else:
-			valid_query = False
-			break
-		
-	if (valid_query):
-		print("Valid Query")
-
-		for value in data:
-			print(value[0] + " | " + value[1])
-
-		# TODO: Print query output
-
-	else:
-		print("Invalid query syntax encountered")
+			print("Invalid query syntax encountered")
 
 	# query()
 
