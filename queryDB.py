70d10f1434074815692bd9bb216c85a55453fdb3
Unknown <ltyue@ualberta.ca>
2018-11-24 20:57:20 -0700

    Querying price, date, and camera work individually
    date is not fixed yet in queries1 and so that does not actually work yet

--- queryDB.py
+++ queryDB.py
@@ -6,36 +6,30 @@ import re
 def main():
 
 	search = input("Input Query: ").lower()
-
 	search_words = search.split()
-
 	num_words = len(search_words)
-
 	valid_query = True
-
 	current_word = 0
-	while current_word < num_words:
-		print(search_words[current_word])
 
+	while current_word < num_words:
+		# print(search_words[current_word])
 		word = search_words[current_word]
 		
 		# check if query is camera
 		if ("camera" in word):
 			if (word == "camera" or word == "camera%"):
 				query(word)
+				current_word += 1
 			else:
 				valid_query = False
 				break
 
 		# check if query is date
-		elif("date" in word):
+		elif (word == "date"):
 			operator = search_words[current_word + 1]
-
 			target = search_words[current_word + 2]
 
-			
-			if (operator == "<" or  operator == ">" or operator == "<=" or operator == ">=" or operator == "=" ):
-
+			if check_operator(operator):
 				
 				try: 
 					year,month,day = target.split('/')
@@ -45,14 +39,41 @@ def main():
 					valid_query = False
 					break
 
-				search_query = word + " " + operator + " " + target
-
-				print(search_query)
-
+				# search_query = word + " " + operator + " " + target
+				search_query = word + operator + target
+				# print(search_query)
 				query(search_query)
+				current_word += 3	
 
+			else:
+				valid_query = False
+				break
 
-		current_word += 1
+		# check if query is price
+		elif (word == "price"):
+			operator = search_words[current_word + 1]
+			target = search_words[current_word + 2]
+
+			if (check_operator(operator)):
+				try:
+					int(target)
+
+				except ValueError:
+					valid_query = False
+					break
+
+				search_query = word + operator + target
+				query(search_query)
+				current_word += 3
+
+			else:
+				valid_query = False
+				break
+
+		else:
+			valid_query = False
+			break
+		
 	if (valid_query):
 		print("Valid Query")
 
@@ -62,5 +83,14 @@ def main():
 		print("Invalid query syntax encountered")
 
 	# query()
+
+
+def check_operator(operator):
+	
+	if (operator == "<" or  operator == ">" or operator == "<=" or operator == ">=" or operator == "="):
+		return True
+
+	else:
+		return False
 
 main()
